3133746d9e4772e7355e46f3d580a5301feb22e8
farahdaghlass <dev.farah.daghlass@gmail.com>
2023-12-20 19:00:43 -1000

    js

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector("div").textContent = "hello , my name is farah.";
+document.querySelector("div").classList.add("demo");
+
+
+document.querySelector("h1").textContent = "im front end developer";
+document.querySelector("h1").classList.remove("vip");
